9fdff96ee57011edd4657f83b8a1766a73c6e008
mbenson987 <megan.benson@tokiomarinekiln.com>
2025-06-06 11:44:42 +0100

    Change colour to blue

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "red") +
+  geom_point(color = "blue") +
   ggtitle("Scatterplot for Sydney temperature and humidity")
 
 # Save the plot in graphics/
