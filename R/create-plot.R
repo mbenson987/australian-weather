f9995b97f47215a9d57384564165ba70c156ec21
mbenson987 <megan.benson@tokiomarinekiln.com>
2025-06-09 09:42:14 +0100

    Recap of chapter 3

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "blue") +
+  geom_point(color = "green") +
   ggtitle("Scatterplot for Sydney temperature and humidity") +
   theme_minimal() + 
   facet_wrap(~WindDir9am)
